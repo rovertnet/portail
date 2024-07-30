996926f65c33486dfc4d6f6b4783dc09997a096b
Robert Matundu <matundukabamba@gmail.com>
2024-07-31 01:00:25 +0200

    last commit

--- src/component/banner/Banner.jsx
+++ src/component/banner/Banner.jsx
@@ -2,8 +2,12 @@
 export default function Banner() {
   return (
     <>
-      <div className="block ">
-        
+      <div className="block sm:px-14 px-14">
+        <div className="flex justify-between items-center sm:flex-col">
+          <img src="../../../public/b1.png" alt="bannerimg" className="" />
+          {/* texte */}
+          <div className=""></div>
+        </div>
       </div>
     </>
   )
--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -5,20 +5,20 @@ import { IoMdSunny } from "react-icons/io";
 export default function Navbar() {
   return (
     <>
-      <div className="block border-b-[1px] border-slate-500 sm:px-14 px-14 sm:py-6 py-5">
+      <div className="block border-b-[0.1px] border-slate-700 sm:px-14 px-14 sm:py-6 py-5 shadow-md fixed top-0 right-0 left-0">
         <div className="flex justify-between items-center">
           <h1 className="text-white text-xl sm:text-lg font-bold">Elearning</h1>
 
           {/* items menu */}
           <div className="flex justify-center items-center gap-3">
-            <ul className=" flex justify-center items-center gap-5 text-xl text-slate-200">
+            <ul className=" flex justify-center items-center gap-5 text-xl text-slate-400">
               <li>Accueil</li>
               <li>Cours</li>
               <li>A propos</li>
               <li>Faq</li>
             </ul>
-            <IoMdSunny className="text-2xl text-slate-200" />
-            <FaRegUserCircle className="text-2xl text-slate-200" />
+            <IoMdSunny className="text-xl text-slate-400" />
+            <FaRegUserCircle className="text-2xl text-slate-400" />
           </div>
         </div>
       </div>
